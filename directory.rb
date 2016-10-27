df5448fd811e47d3ac51dc4330b8d606842722c8
evebalog <evebalog@netscape.net>
2016-10-27 12:10:17 +0100

    Asking for user input

--- directory.rb
+++ directory.rb
@@ -1,24 +1,43 @@
 # no need for this variable: student_count = 11
 # let's put all students into an array
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november}
-]
-# and then print them
+#students = [
+#  {name: "Dr. Hannibal Lecter", cohort: :november},
+#  {name: "Darth Vader", cohort: :november},
+#  {name: "Nurse Ratched", cohort: :november},
+#  {name: "Michael Corleone", cohort: :november},
+#  {name: "Alex DeLarge", cohort: :november},
+#  {name: "The Wicked Witch of the West", cohort: :november},
+#  {name: "Terminator", cohort: :november},
+#  {name: "Freddy Krueger", cohort: :november},
+#  {name: "The Joker", cohort: :november},
+#  {name: "Joffrey Baratheon", cohort: :november},
+#  {name: "Norman Bates", cohort: :november}
+#]
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # create an empty array
+  students = []
+  # get the first name
+  name = gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+    #add the student hash to the array
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    # get another name from the user
+    name = gets.chomp
+  end
+  # return the array of students
+  students
+end
+
+# and then write a method to print a header
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
 end
-
+# method to print student names and their cohort
 def print(students)
   students.each do |student|
   puts "#{student[:name]} (#{student[:cohort]} cohort)"
@@ -28,6 +47,9 @@ end
 def print_footer(students)
   puts "Overall, we have #{students.count} great students"
 end
+
+# get list of students from the user
+students = input_students
 #it's important that print() doesn't add new line characters
 #nothing happens until we call the methods
 print_header
